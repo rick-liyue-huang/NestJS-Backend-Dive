a73145acbbc152f2021c503591b3f6e13c9a35e0
Rick Huang <rickhuang668@Ricks-MacBook-Pro.local>
2023-07-05 20:45:47 +1000

    create mysql in docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
-
 import Configuration from './configuration';
+import * as Joi from 'joi';
 
 @Module({
   // add environment variables configuration
@@ -15,6 +15,9 @@ import Configuration from './configuration';
       // envFilePath: envFilePath,
       // load: [() => dotenv.config({ path: envFilePath })],
       load: [Configuration], // use yaml to config the environment variables
+      validationSchema: Joi.object({
+        DB_PORT: Joi.number().default(3306),
+      }),
     }),
   ],
   controllers: [AppController],
