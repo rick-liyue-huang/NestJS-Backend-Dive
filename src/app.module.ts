548dc78d55d2cfd7109623ee98a00105631306a3
Rick Huang <rickhuang668@Ricks-MacBook-Pro.local>
2023-07-05 17:57:50 +1000

    add config files to set environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+
+import Configuration from './configuration';
 
 @Module({
-  imports: [],
+  // add environment variables configuration
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // let other modules access the configuration variables
+      // envFilePath: envFilePath,
+      // load: [() => dotenv.config({ path: envFilePath })],
+      load: [Configuration], // use yaml to config the environment variables
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
